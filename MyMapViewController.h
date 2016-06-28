31efafbd0411f618c5b9c47a699ee8348648018e
hwoo3303 <hwoo3303@naver.com>
2016-06-28 12:32:11 +0900

    Test1

---
+++ MyMapViewController.h
@@ -0,0 +1,13 @@
+//
+//  MyMapViewController.h
+//  Test1
+//
+//  Created by MyMac on 2016. 6. 28..
+//  Copyright © 2016년 MyMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyMapViewController : UIViewController
+
+@end
---
+++ MyMapViewController.m
@@ -0,0 +1,37 @@
+//
+//  MyMapViewController.m
+//  Test1
+//
+//  Created by MyMac on 2016. 6. 28..
+//  Copyright © 2016년 MyMac. All rights reserved.
+//
+
+#import "MyMapViewController.h"
+
+@interface MyMapViewController ()
+
+@end
+
+@implementation MyMapViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
